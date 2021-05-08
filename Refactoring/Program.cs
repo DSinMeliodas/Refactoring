01c5d5a5706ef655c0a503c076abbfacfcebb07b
DSinMeliodas <58990548+DSinMeliodas@users.noreply.github.com>
2021-05-08 19:41:36 +0200

    refactored Main method - removed unecessary local variables in the Program class - placed each method in their own row

--- Refactoring/Program.cs
+++ Refactoring/Program.cs
@@ -6,17 +6,16 @@ namespace Refactoring
     {
         static void Main()
         {
-            string result;
             Console.WriteLine("Welcome to the Movie Store");
             Movie movie1 = new Movie("movie1", 1);
             Movie movie2 = new Movie("movie2", 2);
             Rental rentalMovie1 = new Rental(movie1, 10);
             Rental rentalMovie2 = new Rental(movie2, 5);
             Customer joe = new Customer("joe");
-            joe.AddRental(rentalMovie1); joe.AddRental(rentalMovie2);
+            joe.AddRental(rentalMovie1); 
+            joe.AddRental(rentalMovie2);
             Console.WriteLine("Let's get the Statement");
-            result = joe.Statement();
-            Console.WriteLine(result);
+            Console.WriteLine(joe.Statement());
         }
     }
 }
